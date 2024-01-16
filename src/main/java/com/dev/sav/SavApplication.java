b1c61996cb8f8b881acb160d08c584b4ac6ffa27
Youness hadide <youness.hadide@gmail.com>
2024-01-16 21:22:32 +0100

    Initial Commit

---
+++ src/main/java/com/dev/sav/SavApplication.java
@@ -0,0 +1,13 @@
+package com.dev.sav;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SavApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SavApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dev/sav/SavApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev.sav;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SavApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
